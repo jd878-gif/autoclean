7ac052e4efc51690ace4e932bdf18b35076949cf
jd878 <jd.878@njit.edu>
2025-12-25 15:30:21 -0500

    Add packaging configuration

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="autoclean",
+    name="autoclean-jeet",  # use unique name if autoclean taken
     version="0.1.0",
     author="Jeet Dave",
-    author_email="jd.878@njit.edu",
     description="Automated data cleaning library for pandas DataFrames",
-    long_description=open("readme.md").read(),
+    long_description=open("readme.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/jd878-gif/autoclean",
     packages=find_packages(),
     install_requires=[
         "pandas>=1.3",
-        "numpy>=1.20"
+        "numpy>=1.21"
     ],
     python_requires=">=3.8",
 )
